0cc5e4e2bf3b448550e11760acfce44384559794
Dong Hur <dhur@college.harvard.edu>
2018-11-18 14:01:40 -0500

    incorporates react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,61 +1,39 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
 
-class App extends Component {
+import Home from './Pages/Home';
+import Blog from './Pages/Blog';
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-    // ReactGA.initialize('UA-110570651-1');
-    // ReactGA.pageview(window.location.pathname);
+// class App extends Component {
 
-  }
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       foo: 'bar',
+//       resumeData: {}
+//     };
+//   }
 
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <Route exact path='/' component={Home}/>
+// {/*          <Route exact path='/blog' component={Blog}/>*/}
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
 
-  componentDidMount(){
-    this.getResumeData();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <Route exact path='/' component={Home}/>
+      <Route path='/blog' component={Blog} />
+    </div>
+  </Router>
+);
 
 export default App;
